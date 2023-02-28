5017da9deb9b9d6c330c03541d8462fd6ab8c92d
Vincent Lin <vinlin24@outlook.com>
2023-02-27 21:08:40 -0800

    Refactor parsing into helper and improve DETACHED

--- branch_state.c
+++ branch_state.c
@@ -30,6 +30,7 @@
 #define MAGENTA "\x1b[35m"
 #define CYAN "\x1b[36m"
 #define WHITE "\x1b[37m"
+#define DIM "\x1b[2m"
 #define END "\x1b[0m"
 
 /* Flags for branch states.  */
@@ -39,10 +40,68 @@
 #define STAGED (1 << 2)
 #define MODIFIED (1 << 3)
 
+#define HEAD_DETACHED (1 << 4)
+#define FATAL (1 << 5)
+
 typedef unsigned char state_t;
 typedef char const *color_t;
 
 /* Helper functions.  */
+
+state_t parse_status(FILE *fp, char *branch_name)
+{
+    state_t state = 0x00;
+    bool branch_found = false;
+
+    char line_buffer[MAX_LINE_LENGTH];
+    while (fgets(line_buffer, sizeof(line_buffer), fp) != NULL)
+    {
+        if (STARTSWITH(line_buffer, "fatal"))
+        {
+            state |= FATAL;
+            return state;
+        }
+
+        if (!branch_found)
+        {
+            if (sscanf(line_buffer, "On branch %s", branch_name))
+            {
+                branch_found = true;
+            }
+            else if (sscanf(line_buffer, "HEAD detached at %s", branch_name))
+            {
+                branch_found = true;
+                state |= HEAD_DETACHED;
+            }
+            continue;
+        }
+
+        if (STARTSWITH(line_buffer, "nothing to commit"))
+        {
+            state |= CLEAN;
+            break;
+        }
+
+        if (STARTSWITH(line_buffer, "Changes not staged for commit") ||
+            STARTSWITH(line_buffer, "Untracked files"))
+        {
+            state |= MODIFIED;
+        }
+        if (STARTSWITH(line_buffer, "Changes to be committed"))
+        {
+            state |= STAGED;
+        }
+        if (STARTSWITH(line_buffer, "Unmerged paths"))
+        {
+            state |= CONFLICT;
+        }
+    }
+
+    if (!branch_found)
+        state |= FATAL;
+
+    return state;
+}
 
 void get_format(char *buffer, color_t *color, state_t state)
 {
@@ -73,75 +132,30 @@ void get_format(char *buffer, color_t *color, state_t state)
 
 int main(int argc, char const *argv[])
 {
+    /* Excuse to use argc lol.  */
+    if (argc > 1)
+        fprintf(stderr, "%s: Ignoring command line arguments...\n", argv[0]);
+
     FILE *fp;
     fp = popen("git status 2>&1", "r");
     if (fp == NULL)
     {
-        fprintf(stderr, "%s: Failed to run `git status`.\n", argv[1]);
+        fprintf(stderr, "%s: Failed to run `git status`.\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     char branch_name[MAX_BRANCH_LENGTH];
-    char line_buffer[MAX_LINE_LENGTH];
+    state_t state = parse_status(fp, branch_name);
 
-    state_t state = 0x00;
-
-    bool branch_found = false;
-    bool head_detached = false;
-    while (fgets(line_buffer, sizeof(line_buffer), fp) != NULL)
+    if (state & FATAL)
     {
-        if (STARTSWITH(line_buffer, "fatal"))
-        {
-            fprintf(stderr, "%s: %s", argv[0], line_buffer);
-            return EINVAL;
-        }
-
-        if (!branch_found)
-        {
-            if (sscanf(line_buffer, "On branch %s", branch_name))
-                branch_found = true;
-            else if (sscanf(line_buffer, "HEAD detached at %s", branch_name))
-            {
-                branch_found = true;
-                head_detached = true;
-            }
-            continue;
-        }
-
-        if (STARTSWITH(line_buffer, "nothing to commit"))
-        {
-            state |= CLEAN;
-            break;
-        }
-
-        if (STARTSWITH(line_buffer, "Changes not staged for commit") ||
-            STARTSWITH(line_buffer, "Untracked files"))
-        {
-            state |= MODIFIED;
-        }
-        if (STARTSWITH(line_buffer, "Changes to be committed"))
-        {
-            state |= STAGED;
-        }
-        if (STARTSWITH(line_buffer, "Unmerged paths"))
-        {
-            state |= CONFLICT;
-        }
-    }
-
-    if (!branch_found)
-    {
-        fprintf(stderr, "%s: Could not extract branch name.\n", argv[0]);
+        fprintf(stderr, "%s: A fatal error occurred.\n", argv[0]);
         return EINVAL;
     }
-    if (head_detached)
-    {
-        // TODO: Make this, like, not be vulnerable to overflow.
-        char with_detached[MAX_BRANCH_LENGTH] = "";
-        strcat(with_detached, "DETACHED:");
-        strcat(with_detached, branch_name);
-        memcpy(branch_name, with_detached, MAX_BRANCH_LENGTH);
-    }
+
+    char detached_prefix[] = DIM "DETACHED:" END;
+    if (!(state & HEAD_DETACHED))
+        detached_prefix[0] = '\0';
 
     /* Zero or more of *, +, !.  Includes the null character.  */
     char symbols[4] = "";
@@ -149,7 +163,7 @@ int main(int argc, char const *argv[])
     get_format(symbols, &color, state);
 
     /* Final output to stdout.  */
-    printf("%s%s%s%s\n", color, branch_name, symbols, END);
+    printf("%s%s%s%s%s\n", detached_prefix, color, branch_name, symbols, END);
 
     pclose(fp);
     return EXIT_SUCCESS;
