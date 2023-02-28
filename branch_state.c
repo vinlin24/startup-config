dc0419d511411299ae245e240773239457d0e110
Vincent Lin <vinlin24@outlook.com>
2023-02-27 22:10:39 -0800

    Convert state flag macros into an enum

--- branch_state.c
+++ branch_state.c
@@ -1,7 +1,12 @@
 /**
  * @file branch_state.c
  * @author Vincent Lin (vinlin24@outlook.com)
- * @brief TODO.
+ * @brief Parse the output of `git status` to determine the state of the current
+ * Git repository, if exists.  Print the current branch name colored and
+ * suffixed with symbols mimicking the VS Code GUI notation (* for modified, +
+ * for staged, etc.).  Written as a performant replacement for my
+ * get_branch_state function in my .bashrc, which was a helper for determining
+ * what to output for the Git part of my custom shell prompt.
  * @date 2023-02-27
  */
 
@@ -33,18 +38,21 @@
 #define DIM "\x1b[2m"
 #define END "\x1b[0m"
 
+typedef char const *color_t;
+
 /* Flags for branch states.  */
 
-#define CLEAN (1 << 0)
-#define CONFLICT (1 << 1)
-#define STAGED (1 << 2)
-#define MODIFIED (1 << 3)
+typedef enum state_t
+{
+    CLEAN = 1 << 0,
+    CONFLICT = 1 << 1,
+    STAGED = 1 << 2,
+    MODIFIED = 1 << 3,
 
-#define HEAD_DETACHED (1 << 4)
-#define FATAL (1 << 5)
+    HEAD_DETACHED = 1 << 4,
+    FATAL = 1 << 5,
 
-typedef unsigned char state_t;
-typedef char const *color_t;
+} state_t;
 
 /* Helper functions.  */
 
