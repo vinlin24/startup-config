0e36ad98691dc95abebc2a1e8cd69ad4cf6a0abb
Vincent Lin <vinlin24@outlook.com>
2023-03-02 03:26:29 -0800

    Implement abbreviating the CWD

--- prompt/user_path.cpp
+++ prompt/user_path.cpp
@@ -4,15 +4,81 @@
 
 #include <cstdlib>
 #include <filesystem>
+#include <optional>
+#include <stdexcept>
+#include <vector>
 
 #include "color.hpp"
+#include "strings.hpp"
 #include "user_path.hpp"
 
 namespace fs = std::filesystem;
 
-static std::string abbreviatePath(fs::path const &path)
+static std::optional<fs::path> relativeFromHome(fs::path const &fullPath)
 {
-    return "/abbreviated/path/TODO"; // TODO.
+    char const *HOME = getenv("HOME");
+    if (HOME == nullptr)
+        return std::nullopt;
+
+    std::optional<std::string>
+        result = substringAfter(fullPath.string(), HOME);
+
+    if (!result.has_value())
+        return std::nullopt;
+    std::string &subpath = result.value();
+
+    /* Exclude leading separator.  */
+    if (!subpath.empty())
+        subpath = subpath.substr(1);
+    return fs::path(subpath);
+}
+
+static void appendLastTwoComponents(fs::path &base, fs::path const &source)
+{
+    std::vector<fs::path> components;
+    for (fs::path const &component : source)
+        components.push_back(component);
+
+    if (components.empty())
+        return;
+    if (components.size() <= 2)
+    {
+        base /= source;
+        return;
+    }
+
+    fs::path &current = components.end()[-1];
+    fs::path &parent = components.end()[-2];
+    base = base / "..." / parent / current;
+}
+
+static std::string abbreviatePath(fs::path const &fullPath)
+{
+    fs::path abbreviation;
+
+    std::optional<fs::path> result = relativeFromHome(fullPath);
+    fs::path subpath;
+
+    /* We're under the home directory, so use ~ as the first  */
+    if (result.has_value())
+    {
+        subpath = result.value();
+        abbreviation += "~";
+    }
+
+    /* We're not under the home directory, so use root as the first.  */
+    else
+    {
+        fs::path root = fullPath.root_path();
+        abbreviation += root;
+
+        fs::path afterRoot = fs::relative(fullPath, root);
+        if (afterRoot.string() != ".")
+            subpath = afterRoot;
+    }
+
+    appendLastTwoComponents(abbreviation, subpath);
+    return abbreviation.string();
 }
 
 int getUserPath(std::string &userPath)
