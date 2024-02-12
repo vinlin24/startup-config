0ac5eaa81a340b32133e86745db45e77909b562a
Vincent Lin <vinlin24@outlook.com>
2024-02-11 22:30:35 -0800

    Update setup script
    Tried using it on a new WSL distro recently and didn't work as expected.
    Both paths seemed to be wrong after reviewing a dotfiles tutorial video
    at: https://www.youtube.com/watch?v=r_MpUP6aKiQ.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+repo_dir="${HOME}/repos/startup-config"
+
 # Create symlinks for dotfiles.
-ln -sf "${HOME}/.bashrc" .bashrc
-ln -sf "${HOME}/.bash_profile" .bash_profile
-ln -sf "${HOME}/.gitconfig" .gitconfig
-ln -sf "${HOME}/.gitfuncs" .gitfuncs
+ln -sf "${repo_dir}/.bashrc" "${HOME}/.bashrc"
+ln -sf "${repo_dir}/.bash_profile" "${HOME}/.bash_profile"
+ln -sf "${repo_dir}/.gitconfig" "${HOME}/.gitconfig"
+ln -sf "${repo_dir}/.gitfuncs" "${HOME}/.gitfuncs"
 
 # Update our custom bin directory for custom scripts/binaries.
-mkdir "${HOME}/bin"
+mkdir -p "${HOME}/bin"
 make all
 cp bin/* "${HOME}/bin"
 make clean
